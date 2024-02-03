8b0b75a5c0f50de5429a32b758798deece21b9d7
VICTOR-M8 <darkhink8@gmail.com>
2024-02-03 17:51:57 +0100

    new change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://VICTOR-M8.github.io/My-Portfolio",
 })
