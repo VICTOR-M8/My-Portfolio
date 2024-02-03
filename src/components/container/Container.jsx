423b912717e631039162629ea935efbe0fbc2dcb
VICTOR-M8 <darkhink8@gmail.com>
2024-02-03 22:50:40 +0100

    new change

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,12 @@
+import "../container/Container.css"
+const Container = () => {
+    return (
+        <div>
+            
+        <h1 className="title">My portfolio</h1>
+
+    </div>
+    )
+}
+
+export default Container
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import Container from "../components/Container/Container"
 const Home = () => {
     return (
-        <h1>welcome to my portfolio</h1>
+        <> 
+            <Container/>
+        </>
+
+        
     )
 }
 
